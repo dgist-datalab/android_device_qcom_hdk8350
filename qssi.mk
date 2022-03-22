57b3b7e8239dd8f2a4b10df37778521421161279
Juhyung Park <qkrwngud825@gmail.com>
2022-03-23 03:37:20 +0900

    hdk8350: add fstab for unencrypted userdata mount
    Change-Id: I290350f30c23b56deeba4f2bc575099e22bd35d1
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- qssi.mk
+++ qssi.mk
@@ -263,6 +263,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.crypto.volume.filenames_mode = "aes-256-cts" \
 ro.crypto.allow_encrypt_override = true
 
+PRODUCT_PACKAGES += \
+    fstab.unencrypted \
+    fstab.unencrypted.late-fs \
+    init.hdk8350.rc
+
 TARGET_USES_QCOM_DISPLAY_BSP := true
 
 ifeq ($(TARGET_USES_NEW_ION),true)
